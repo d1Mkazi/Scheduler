92de65277207dfd63cdbc57c380b351417d607bd
d1Mkazi <dmitriivkyt@gmail.com>
2025-09-22 21:36:24 +0600

    Accessed window from C++

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,18 +1,30 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "mainwindow.h"
+
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qt/gui/main.qml"));
     QObject::connect(
         &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
+        &QQmlApplicationEngine::objectCreated,
         &app,
-        []() { QCoreApplication::exit(-1); },
+        [url](QObject *obj, const QUrl &objUrl) {
+            if(!obj && url == objUrl) {
+                QCoreApplication::exit(1);
+            }
+        },
         Qt::QueuedConnection);
-    engine.load("qt/gui/main.qml");
+    engine.load(url);
+
+    QObject *window = engine.rootObjects()[0];
+    if(!window) {
+        QCoreApplication::exit(2);
+    }
 
     return app.exec();
 }
